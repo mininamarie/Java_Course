f229ed1feedb2f87780f77aa20d8706acc368fde
Mariia <mininamarie@gmail.com>
2021-09-16 19:21:12 -0400

    Class 1 files

---
+++ src/ca/concordia/fallterm/Class_01/HelloWorld.java
@@ -0,0 +1,9 @@
+package ca.concordia.fallterm.Class_01;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/ca/concordia/fallterm/Main.java
@@ -0,0 +1,8 @@
+package ca.concordia.fallterm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
